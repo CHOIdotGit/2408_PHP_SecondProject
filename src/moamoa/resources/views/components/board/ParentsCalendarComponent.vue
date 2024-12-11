bc9d8c70fbd68a2160b8778748cb591c1ea8e27c
bhj017673 <baehyunjinny48@gmail.com>
2024-12-11 18:25:33 +0900

    1211 배현진 부모 캘린더

--- src/moamoa/resources/views/components/board/ParentsCalendarComponent.vue
+++ src/moamoa/resources/views/components/board/ParentsCalendarComponent.vue
@@ -9,10 +9,10 @@
             </div>
             <div class="blank"></div>
             <div  class="btn-box">
-                <button class="content-btn">내역</button>
-                <button class="data-btn">통계</button>
+                <button class="content-btn" :class="{ active: showHistory }"  @click="showHistory = true">내역</button>
+                <button class="data-btn" :class="{ active: !showHistory }"  @click="showHistory = false">통계</button>
             </div>
-            <div class="money-history">
+            <div v-show="showHistory" class="money-history" >
                 <div class="money-title">
                     <li>식비</li>
                     <li>교통비</li>
@@ -26,7 +26,7 @@
                     <ul>
                         <li class="cost">7,000원</li>
                         <li class="cost">15,000원</li>
-                        <li class="cost">12,000</li>
+                        <li class="cost">12,000원</li>
                         <li class="cost">8,000원</li>
                         <li class="cost">15,000원</li>
                         <li class="cost">40,000원</li>
@@ -34,52 +34,101 @@
                     </ul>
                 </div>
             </div>
+            <div v-show="!showHistory" class="data-chart">
+                <p>차트표시</p>
+            </div>
         </div>
         
-        
-        <div class="sec-container">
-            <div class="calendar">
-                <div class="sec-header">
-                    <pre class="left"> ◀ </pre>
-                    <div class="header-display">
-                        <p class="display"></p>
+        <div class="cal-sec-container">
+            <div class="sec-container">
+                <div class="calendar">
+                    <div class="sec-header">
+                    <!-- 이전/다음 버튼 -->
+                        <pre class="left" @click="prevMonth">◀</pre>
+                            <div class="header-display">
+                                <!-- 현재 연월 표시 -->
+                                <p class="display">{{ formattedDate }}</p>
+                            </div>
+                        <pre class="right" @click="nextMonth">▶</pre>
                     </div>
-                    <pre class="right"> ▶ </pre>
+                    <div class="week">
+                        <div style="color: red;">일</div>
+                        <div>월</div>
+                        <div>화</div>
+                        <div>수</div>
+                        <div>목</div>
+                        <div>금</div>
+                        <div style="color: blue;">토</div>
+                    </div>
+                    <div class="days">
+                        <!-- 시작 요일 빈 칸 -->
+                        <div v-for="n in startDay" :key="'empty-' + n" class="day empty"></div>
+                        <!-- 날짜 표시 -->
+                        <div v-for="day in daysInMonth" :key="day" class="day">
+                            {{ day }} 
+                            <p class="minus">-5,000</p>
+                            <p class="plus">+3,000</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="week">
-                    <div>월</div>
-                    <div>화</div>
-                    <div>수</div>
-                    <div>목</div>
-                    <div>금</div>
-                    <div>토</div>
-                    <div>일</div>
-                </div>
-                <div class="days">
-                    <!-- days will be filled here -->
-                    
-                </div>
-            
-            <div class="display-selected">
-                <p class="selected">
-                    
-                </p>
             </div>
         </div>
     </div>
-</div>
 </template>
 
 
 <script setup>
+
+const showHistory = ref(true);
+
+
+import { ref, computed } from "vue";
+
+// 현재 날짜 상태 관리
+const dateToday = ref(new Date());
+
+// 현재 연월 표시 (반응형 데이터)
+const formattedDate = computed(() =>
+  dateToday.value.toLocaleString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    timeZone: "Asia/Seoul",
+  })
+);
+
+// 해당 월의 시작 요일 계산
+const startDay = computed(() => {
+  const firstDayOfMonth = new Date(dateToday.value.getFullYear(), dateToday.value.getMonth(), 1);
+  return firstDayOfMonth.getDay(); // 0: 일요일, 1: 월요일, ..., 6: 토요일
+});
+
+// 해당 월의 일수 계산
+const daysInMonth = computed(() => {
+  const year = dateToday.value.getFullYear();
+  const month = dateToday.value.getMonth();
+  return new Array(new Date(year, month + 1, 0).getDate()).fill(null).map((_, i) => i + 1);
+});
+
+// 이전 월로 이동
+function prevMonth() {
+  const currentDate = new Date(dateToday.value);
+  currentDate.setMonth(currentDate.getMonth() - 1);
+  dateToday.value = currentDate; // 새로운 객체로 업데이트
+}
+
+// 다음 월로 이동
+function nextMonth() {
+  const currentDate = new Date(dateToday.value);
+  currentDate.setMonth(currentDate.getMonth() + 1);
+  dateToday.value = currentDate; // 새로운 객체로 업데이트
+}
+
 
 
 </script>
 
 
 <style>
-
-
 .cal-container {
     width: 93%;
     display: flex;
@@ -128,17 +177,21 @@
     width: 140px;
     height: 50px;
     margin-left: 20px;
+    line-height: 50px;
     font-size: 2rem;
     border: none;
-    background-color: #cacaca;
+    color: white;
+    background-color: #B0BFC1;
 }
 
 .data-btn  {
     width: 140px;
     height: 50px;
+    line-height: 50px;
     font-size: 2rem;
     border: none;
-    background-color: #737373;
+    color: white;
+    background-color: #B0BFC1;
 }
 
 li {
@@ -152,25 +205,31 @@ li {
 
 .money-history {
     width: 400px;
-    height: 490px;
+    height: 520px;
     margin-left: 20px;
     margin-right: 20px;
     display: grid;
     grid-template-columns: 1fr 1fr;
     font-size: 1.5rem;
     line-height: 50px;
-    background-color: #cacaca;
+    background-color: #E2F1F3;
 }
 
+.data-chart {
+    width: 400px;
+    height: 520px;
+    margin-left: 20px;
+    margin-right: 20px;
+    background-color: #E2F1F3;
+}
 
 .cost  {
     list-style-type: none;
+    text-align: right;
+    padding-right: 50px;
 }
 
-/* .calendar {
-    background-color: white;
-    width: 1300px;
-} */
+
 
 :root {
     --white: white;
@@ -180,20 +239,24 @@ li {
 }
 
 .sec-container {
-    display: flex;
-    height: 500px;
+    /* display: flex; */
+    height: 700px;
     justify-content: center;
     align-items: center;
-    background: var(--main);
+    background: var(--white);
 }
 
-.calendar {
-    background: var(--white);
-    border-radius: 25px;
+.cal-sec-container {
+    width: 1290px;
+    height: 750px;
+    background: var(--white);   
     padding: 0 1em;
 }
 
 .sec-header {
+    /* position: sticky; */
+    height: 115px;
+    line-height: 115px;
     display: flex;
     justify-content: space-between;
     padding: 10px ;
@@ -203,7 +266,7 @@ li {
     display: flex;
     justify-content: center;
     align-items: center;
-    font-size: 1.5rem;
+    font-size: 3rem;
     color: var(--accent-2);
     word-spacing: 0.5rem;
 }
@@ -215,31 +278,49 @@ pre {
     cursor: pointer;
 }
 
-.days, .week {
+.days{
     display: grid;
     grid-template-columns: repeat(7, 1fr);
     padding: 0 20px;
     justify-content: space-between;
+    align-items: center;
+    padding-top: 15px;
     div{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 3rem;
-        height: 3rem;
+        text-align: center;
+        font-size: 2rem;
+        width: 168px;
+        height: 130px;
         opacity: 0.5;
     }
-    
 }
+
+.week{
+    
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    padding: 0 20px;
+    justify-content: space-between;
+    align-items: center;
+    div{
+        height: 55px;
+        font-size: 2rem;
+        text-align: center;
+        line-height: 55px;
+        border-bottom: 7px double #ffBDD0;
+    }
+}
+
 .days div{
     
     border-radius: 50%;
     opacity: 1;
-    &:hover {
+    height: 95px;
+    /* &:hover {
         background: var(--accent-2);
         color: #737373;
         cursor: pointer;
         
-    }
+    } */
 }
 
 .display-selected {
@@ -253,4 +334,28 @@ pre {
     color: var(--white);
 } 
 
+.right, .left {
+    font-size: 3rem;
+    line-height: 3rem;
+    line-height: 95px;
+}
+
+.minus {
+    color: red;
+    font-size: 0.9rem;
+}
+
+.plus {
+    color: blue;
+    font-size: 0.9rem;
+}
+
+.active {
+  background-color: #E2F1F3; /* 활성화된 버튼 배경색 */
+  color: black;
+}
+
+.money-title, .money-cost {
+    margin-top: 15px;
+}
 </style>
