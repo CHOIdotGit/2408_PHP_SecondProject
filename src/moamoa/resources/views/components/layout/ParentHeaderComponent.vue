416d0a892a08489c6b6ffc1b81824a8f894a7a43
iamjuyeon <juyoon57@naver.com>
2025-01-08 14:49:46 +0900

    0108 김주연 부모 헤더 통계 수정

--- src/moamoa/resources/views/components/layout/ParentHeaderComponent.vue
+++ src/moamoa/resources/views/components/layout/ParentHeaderComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="item" v-for="item in baseMenuInfo" :key="item">
         <router-link :to="item.path + item.segmentPath" @click="$store.dispatch(item.actionName, firstChildId)" class="link-deco"><p class="item-btn">{{ item.name }}</p></router-link>
-        <div class="child-dropdown" v-if="$store.state.auth.parentFlg && item.name !== '홈' && item.name !=='통계' " v-for="child in childNameList" :key="child">
+        <div class="child-dropdown" v-if="$store.state.auth.parentFlg && item.name !== '홈' " v-for="child in childNameList" :key="child">
             <router-link :to="item.path + '/' + child.child_id" @click="$store.dispatch(item.actionName, child.child_id)" class="link-deco"><p class="child" >{{ child.name }}</p></router-link>
         </div>
         <!-- <div class="child-dropdown" v-if="$store.state.auth.parentFlg" v-for="child in childNameList" :key="child">
@@ -29,7 +29,7 @@ onBeforeMount(async () => {
         ,{name: "지출", path: "/parent/spend/list", segmentPath: '/' + firstChildId.value, actionName: 'transaction/transactionList'}
         ,{name: "미션", path: "/parent/mission/list", segmentPath: '/' + firstChildId.value, actionName: 'mission/missionList'}
         ,{name: "캘린더", path: "/parent/calendar", segmentPath: '/' + firstChildId.value, actionName: 'calendar/calendarInfo'}
-        ,{name: "통계", path: "/parent/stats", segmentPath: ''}
+        ,{name: "통계", path: "/parent/stats", segmentPath: '/' + firstChildId.value, actionName: 'transaction/childHomeTransaction'}
     ];
     console.log('애들이름출력');
 })
